ab9c175ea8a439c5af4e3433360fab73d922b508
Olivier St-Amand <olivier.st.amand.1@gmail.com>
2024-04-16 13:05:30 -0400

    Add test for not valid register code

--- tests/test_register.py
+++ tests/test_register.py
@@ -37,3 +37,14 @@ def test_can_create_user_with_valid_key(test_db):
 
     delete_from_db(test_db, register_code)
     delete_from_db(test_db, user)
+
+
+def test_register_code_not_valid(test_db):
+    username = get_random_string(10)
+    password = get_random_string(10)
+    register_code = get_random_string(12)
+
+    ok, data = register_user(test_db, username, password, register_code)
+
+    assert not ok
+    assert "detail" in data
